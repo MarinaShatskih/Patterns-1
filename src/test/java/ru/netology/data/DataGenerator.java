f46e819ccfd2155bce07196bc91e3d77602572a0
Marina <butkevich.marina7896@gmail.com>
2026-03-06 12:42:36 +0700

    first commit

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,42 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        var cities = new String[]{"Москва", "Иркутск", "Новосибирск", "Нижний Новгород", "Санкт-Петербург"};
+        return cities[(int) (Math.random() * cities.length)];
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String name = faker.name().fullName();
+        return name;
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String phone = String.valueOf(faker.phoneNumber());
+        return phone;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
+    }
+}
